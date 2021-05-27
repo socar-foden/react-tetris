a6484d72c39f50f3be2e35a75b3ee08312dac999
brandon <zxczoxc125@naver.com>
2021-05-28 08:16:35 +0900

    # 원복

--- src/models/spaces.ts
+++ src/models/spaces.ts
@@ -1,13 +1,9 @@
-import { v4 as uuidv4 } from "uuid";
-
 import { Block } from "./blocks";
 
 export class Space {
   _block: Block | null;
-  _id: string;
 
   constructor(block?: Block) {
-    this._id = uuidv4();
     this._block = block;
   }
 }
